d45c53ba9782f1e9114348bd588c511fabec3061
aryanmathur1 <134450794+aryanmathur1@users.noreply.github.com>
2025-05-17 10:20:51 -0400

    Initial Commit

---
+++ Time Machine/Time_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_MachineApp.swift
+//  Time Machine
+//
+//  Created by Aryan Mathur on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Time_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Time MachineTests/Time_MachineTests.swift
@@ -0,0 +1,17 @@
+//
+//  Time_MachineTests.swift
+//  Time MachineTests
+//
+//  Created by Aryan Mathur on 5/17/25.
+//
+
+import Testing
+@testable import Time_Machine
+
+struct Time_MachineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
